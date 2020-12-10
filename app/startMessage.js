984c1cea122e5f38c26681b42ee1f428086d5fc6
kiambati-maxwel <antidoteprog@gmail.com>
2020-12-10 18:02:54 +0300

    emptying Master branch

--- app/startMessage.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-
-const set_mode = (mode = 'dev') => {
-  let app_mode = 'dev';
-  mode === 'prod' ? app_mode = 'prod' : app_mode = 'dev';
-  return app_mode;
-}
-export default set_mode;
--- test/start_script.test.js
+++
@@ -1,9 +0,0 @@
-import set_mode from '../app/startMessage';
-import assert from 'assert';
-
-describe('it tests application mode', () => {
-  it('verifies mode set', done => {
-    assert.equal('dev', set_mode());
-    done();
-  });
-});
