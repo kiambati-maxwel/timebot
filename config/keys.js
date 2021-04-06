71f452d234554e73afbe7ff16a11e6215de7801d
kiambati-maxwel <antidoteprog@gmail.com>
2021-04-06 22:33:18 +0300

    adding core js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 const connectionString = {
-  mongoURL: "mongodb://localhost",
+  // mongoURL: 'mongodb://localhost'
+  mongoURL:
+    "mongodb+srv://netbot:mongodb6595@cluster0.khehv.gcp.mongodb.net/timebot?retryWrites=true&w=majority",
 };
 
 module.exports = connectionString;
